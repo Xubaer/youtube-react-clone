a9dec8d5a236956358d3a41dc96642f3ba6c11e7
xubaer <xubaer@hotmail.com>
2019-03-11 03:44:25 +0500

    added app layout

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { VideoGrid } from '../../components/VideoGrid/VideoGrid';
+import {SideBar} from '../SideBar/SideBar';
 import './Home.scss';
 
 export class Home extends React.Component {
     state = {  }
     render() {
         return (
-            <div className='home'>
-                <VideoGrid title='Trending'/>
-                <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
-            </div>
+            <React.Fragment>
+                <SideBar />
+                <div className='home'>
+                    <VideoGrid title='Trending'/>
+                    <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+                </div>
+            </React.Fragment>
         );
     }
 }
